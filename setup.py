5d0869c835c5c778b56d28c06fcbd1a17080f119
odaken <odaken.mobile@gmail.com>
2018-07-22 21:45:15 +0900

    update: submo

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='binance',
-      version='0.2',
-      py_modules=['binance'],
-      description='Binance API wrapper',
-      url='https://github.com/toshima/binance',
-      author='Takaki Oshima')
